2a3f3e48a8c4103ce8e8b94c15ce851b598e3758
Tobias Walter <tobias.walter@ituc.de>
2024-04-17 11:56:39 +0200

    generated stuff

--- src/test/java/de/ketobi/vaadinspringdemo/VaadinspringdemoApplicationTests.java
+++ src/test/java/de/ketobi/vaadinspringdemo/VaadinspringdemoApplicationTests.java
@@ -1,13 +1,28 @@
 package de.ketobi.vaadinspringdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class VaadinspringdemoApplicationTests {
-
+	ApplicationContext applicationContext;
 	@Test
 	void contextLoads() {
 	}
 
+	// Verifies that the application context is not null
+	@Test
+	void contextNotNull() {
+		assertNotNull(applicationContext);
+	}
+
+	@Test
+	void contextContainsBeans() {
+		assertTrue(applicationContext.containsBean("WorkflowRepository"));
+	}
 }
