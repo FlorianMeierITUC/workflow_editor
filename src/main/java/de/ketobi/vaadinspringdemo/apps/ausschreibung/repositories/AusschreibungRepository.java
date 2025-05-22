1f8e183ed7a57dd79e775f8205c9bd19c8ce4718
Emilie Drouot <emilie.drouot.bpi@ituc.de>
2025-05-22 16:01:06 +0200

    archiv page

--- src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/repositories/AusschreibungRepository.java
+++ src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/repositories/AusschreibungRepository.java
@@ -2,6 +2,8 @@ package de.ketobi.vaadinspringdemo.apps.ausschreibung.repositories;
 
 import de.ketobi.vaadinspringdemo.apps.ausschreibung.entities.Ausschreibung;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface AusschreibungRepository extends MongoRepository<Ausschreibung, String> {
+    //List<Ausschreibung> findByArchivedTrue();
 }
