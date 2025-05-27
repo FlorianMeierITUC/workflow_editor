4b5f99162b42a745471d4a40186094dfbd889ac4
Emilie Drouot <emilie.drouot.bpi@ituc.de>
2025-05-27 13:58:56 +0200

    deleted sample data

--- src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/repositories/AusschreibungRepository.java
+++ src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/repositories/AusschreibungRepository.java
@@ -2,8 +2,6 @@ package de.ketobi.vaadinspringdemo.apps.ausschreibung.repositories;
 
 import de.ketobi.vaadinspringdemo.apps.ausschreibung.entities.Ausschreibung;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
 
 public interface AusschreibungRepository extends MongoRepository<Ausschreibung, String> {
-    //List<Ausschreibung> findByArchivedTrue();
 }
