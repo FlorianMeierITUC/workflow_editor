8388e966bd24276ea1f4a03dd8c39167f5e723bc
Emilie Drouot <emilie.drouot.bpi@ituc.de>
2025-05-27 11:13:15 +0200

    correction of the add button css

--- src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/entities/Ausschreibung.java
+++ src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/entities/Ausschreibung.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+<<<<<<< HEAD
 import lombok.Data
 
 import lombok.Data;
