a1d1821594ae9f215e69b93758600f86674d09be
Florian Meier <florian.meier@ituc.de>
2025-06-17 16:58:53 +0200

    Add WebClient configuration class for reactive web client setup

---
+++ src/main/java/de/ketobi/vaadinspringdemo/main/config/WebClientConfig.java
@@ -0,0 +1,15 @@
+package de.ketobi.vaadinspringdemo.main.config;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+
+    }
+
+}
