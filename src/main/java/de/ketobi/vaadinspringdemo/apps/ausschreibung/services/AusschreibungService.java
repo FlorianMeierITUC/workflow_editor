61ed9215f4a7d4c32846bab0d68ac3ff442d576f
Florian Meier <florian.meier@ituc.de>
2025-06-17 18:07:23 +0200

    Refactor AusschreibungService to inject ChatService and remove unused repository code

--- src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/services/AusschreibungService.java
+++ src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/services/AusschreibungService.java
@@ -2,33 +2,19 @@ package de.ketobi.vaadinspringdemo.apps.ausschreibung.services;
 
 import de.ketobi.vaadinspringdemo.apps.ausschreibung.entities.Ausschreibung;
 import de.ketobi.vaadinspringdemo.apps.ausschreibung.repositories.AusschreibungRepository;
-import org.springframework.stereotype.Service;
+import de.ketobi.vaadinspringdemo.main.services.ChatService;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 @Service
 public class AusschreibungService {
-    private final AusschreibungRepository repo;
 
-    public AusschreibungService(AusschreibungRepository repo) {
-        this.repo = repo;
-    }
+    private final ChatService chatService;
 
-    public Ausschreibung save(Ausschreibung ausschreibung) {
-        return repo.save(ausschreibung);
-    }
-
-    public List<Ausschreibung> findAll() {
-        return repo.findAll();
-    }
-
-
-    public void deleteAll() {
-        repo.deleteAll();
-    }
-    
-    public Optional<Ausschreibung> findById(String id) {
-        return repo.findById(id);
+    @Autowired
+    public AusschreibungService(@Qualifier("MainChatService") ChatService chatService) {
+        this.chatService = chatService;
     }
 }
