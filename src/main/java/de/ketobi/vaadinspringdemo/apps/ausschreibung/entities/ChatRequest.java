07b7fa94d42d78d59e4ffdb668c57c4c646bbe3e
Florian Meier <florian.meier@ituc.de>
2025-06-17 16:31:51 +0200

    Add ChatRequest class for handling chat request data

---
+++ src/main/java/de/ketobi/vaadinspringdemo/apps/ausschreibung/entities/ChatRequest.java
@@ -0,0 +1,21 @@
+package de.ketobi.vaadinspringdemo.apps.ausschreibung.entities;
+
+import lombok.Data;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ChatRequest {
+   @JsonProperty("chat_history")
+   private List<Message> chatHistory;
+   @JsonProperty("system_prompt")
+   private String systemPrompt;
+   private float temperature;
+   private String image;
+   private String document;
+}
