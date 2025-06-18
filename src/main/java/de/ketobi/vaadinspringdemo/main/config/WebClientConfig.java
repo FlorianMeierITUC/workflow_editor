2992f3f640b37ec7e2cd03d0ea8057855db62410
Florian Meier <florian.meier@ituc.de>
2025-06-18 19:35:11 +0200

    refactor: add qualifiers to WebClient beans in WebClientConfig

--- src/main/java/de/ketobi/vaadinspringdemo/main/config/WebClientConfig.java
+++ src/main/java/de/ketobi/vaadinspringdemo/main/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package de.ketobi.vaadinspringdemo.main.config;
 
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +10,21 @@ import org.springframework.context.annotation.Configuration;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(@Value("${chat.base-url}") String chatBaseUrl) {
-        return WebClient.builder().baseUrl(chatBaseUrl).build();
+    @Qualifier("chatWebClient")
+    public WebClient chatWebClient(@Value("${chat.base-url}") String BaseUrl) {
+        return WebClient.builder().baseUrl(BaseUrl).build();
+    }
 
+    @Bean
+    @Qualifier("indexingWebClient")
+    public WebClient indexingWebClient(@Value("${indexing.base-url}") String BaseUrl) {
+        return WebClient.builder().baseUrl(BaseUrl).build();
+    }
+
+    @Bean
+    @Qualifier("dataWebClient")
+    public WebClient dataWebClient(@Value("${data.base-url}") String BaseUrl) {
+        return WebClient.builder().baseUrl(BaseUrl).build();
     }
 
 }
