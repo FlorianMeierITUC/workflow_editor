336ba15a5065109d433af046ac11d94cd2f98570
Florian Meier <florian.meier@ituc.de>
2025-06-18 19:40:43 +0200

    feat: add JacksonConfig class for custom ObjectMapper configuration

---
+++ src/main/java/de/ketobi/vaadinspringdemo/main/mappers/JacksonConfig.java
@@ -0,0 +1,18 @@
+package de.ketobi.vaadinspringdemo.main.mappers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper jacksonObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
