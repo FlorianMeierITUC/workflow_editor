d16b537b7bb232d025d34550cddbdee17a14bad8
Jacopo Grotti <jacopo.grotti@ituc.de>
2025-07-18 09:18:02 +0200

    increase webclients maxInMemorySize

--- src/main/java/de/ketobi/vaadinspringdemo/main/config/WebClientConfig.java
+++ src/main/java/de/ketobi/vaadinspringdemo/main/config/WebClientConfig.java
@@ -9,22 +9,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebClientConfig {
 
+    private static final int MAX_IN_MEMORY_SIZE = 10 * 1024 * 1024; // 10 MB
+
     @Bean
     @Qualifier("chatWebClient")
-    public WebClient chatWebClient(@Value("${chat.base-url}") String BaseUrl) {
-        return WebClient.builder().baseUrl(BaseUrl).build();
+    public WebClient chatWebClient(@Value("${chat.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build();
     }
 
     @Bean
     @Qualifier("indexingWebClient")
-    public WebClient indexingWebClient(@Value("${indexing.base-url}") String BaseUrl) {
-        return WebClient.builder().baseUrl(BaseUrl).build();
+    public WebClient indexingWebClient(@Value("${indexing.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build();
     }
 
     @Bean
     @Qualifier("dataWebClient")
-    public WebClient dataWebClient(@Value("${data.base-url}") String BaseUrl) {
-        return WebClient.builder().baseUrl(BaseUrl).build();
+    public WebClient dataWebClient(@Value("${data.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build();
     }
-
 }
